224cb7db1323c6cf40bc594ca0fa7d0b57b0650e
Ben <bc@bencarterconsulting.com>
2024-07-28 11:34:23 -0400

    Assignment possibly complete; may need additional revisions.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   const submit = document.querySelector(#create-task-form)
+//   submit.addEventListener(e);
+//   e.preventDefault();
+//   }
+// });
+
+
+document.addEventListener("#create-task-form", function(e) {
+  let submit = document.querySelector("#submit")
+  Event.preventDefault(submit)
+})
+
+// document.addEventListener("submit", function(event) {
+//   event.preventDefault();
+//   let submit = document.querySelector("#submit");
+// });
+
+// const checkbox = document.querySelector("#id-checkbox");
+
+// checkbox.addEventListener("click", checkboxClick, false);
+
+// function checkboxClick(event) {
+//   const warn = "preventDefault() won't let you check this!\n";
+//   document.getElementById("output-box").innerText += warn;
+//   event.preventDefault();
+// }
